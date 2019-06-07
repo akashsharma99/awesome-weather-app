8651f13bb8be6f20f462a6ca3c5b592cf78c7da0
akashsharma99 <akash.asharma.sharma11@gmail.com>
2019-06-07 23:57:58 +0530

    http to https

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,7 +2,7 @@ const key = 'CENrIPH7HfzGsnbq10x9HA2W0AvNeTFi';
 
 //get weather info
 const getWeather = async (id) => {
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
     const response = await fetch(base+query);
     const data = await response.json();
@@ -13,7 +13,7 @@ const getWeather = async (id) => {
 
 //get city key
 const getCity = async (city) => {
-    const base='http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base='https://dataservice.accuweather.com/locations/v1/cities/search';
     const query=`?apikey=${key}&q=${city}`;
     const response = await fetch(base+query);
     const data = await response.json();
